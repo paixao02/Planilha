fda9f360da0affee1d601fd47847491ee670e0df
paixao02 <82853434+paixao02@users.noreply.github.com>
2026-07-08 17:44:55 -0300

    v12

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='financeapp-ultra-v200';
+const CACHE='financeapp-ultra-v500';
 const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
