ee6cf20506dfbe701f12ee82f6ba270ba8d4b21d
paixao02 <82853434+paixao02@users.noreply.github.com>
2026-07-08 12:16:08 -0300

    Add files via upload

---
+++ default_data.js
@@ -0,0 +1,5 @@
+// Default transactions and categories generated from transacoes_2026-07-08 CSV files
+// Each transaction includes a unique id for editing and deleting
+
+const defaultTransactions = [{/* placeholder will be replaced */}];
+const defaultCategories = [{/* placeholder will be replaced */}];
---
+++ service-worker.js
@@ -0,0 +1,51 @@
+// Simple service worker for offline caching
+const CACHE_NAME = 'finance-app-cache-v1';
+const PRECACHE_ASSETS = [
+  '/',
+  'index.html',
+  'style.css',
+  'script.js',
+  'manifest.json',
+  'icons/icon-192.png',
+  'icons/icon-512.png'
+];
+
+// Install event: pre-cache essential assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(PRECACHE_ASSETS);
+    })
+  );
+});
+
+// Activate event: cleanup old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch event: respond with cached asset if available or fetch from network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).then((networkResponse) => {
+        // Cache the new response for future requests
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    })
+  );
+});
